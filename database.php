ae4b8c3e08c3616d5db52de64f70f5db170a1f9e
anandmingle <37205209+anandmingle@users.noreply.github.com>
2019-02-12 18:01:53 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-    $hostname = "fdb18.awardspace.net";
-    $user = "2566141_anand";
-    $password = "Thezxy@123";
-    $database = "2566141_anand";
+    $hostname = "#";
+    $user = "#";
+    $password = "#";
+    $database = "";
     $prefix = "";
     $database=mysqli_connect($hostname,$user,$password,$database);
 ?>
